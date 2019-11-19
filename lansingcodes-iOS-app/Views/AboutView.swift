d84df38b0fdb5d4c03916b550216b3407dd975d2
Leo Dion <leogdion@brightdigit.com>
2019-11-18 20:31:13 -0500

    added about view

--- lansingcodes-iOS-app/Views/AboutView.swift
+++ lansingcodes-iOS-app/Views/AboutView.swift
@@ -2,12 +2,36 @@ import SwiftUI
 
 struct AboutView: View {
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    VStack {
+      Spacer()
+      Image("Logo").resizable().scaledToFit().frame(minHeight: 40.0, maxHeight: 80.0)
+      Text("LANSING CODES").font(.title)
+      VStack(alignment: .center) {
+        Text("Events and resources for Lansing coders")
+        Divider()
+        Text("For those who code or aspire to, professionally or as a hobby").font(.caption).multilineTextAlignment(.center)
+      }.padding(20.0)
+      Text("Developed By")
+      Text("Leo Dion")
+      Image("BrightDigit").renderingMode(.template).resizable().scaledToFit().frame(minHeight: 40.0, maxHeight: 80.0)
+      Spacer()
+      Text("Version: 1.0-beta.4 (4)").font(.caption).foregroundColor(.gray)
+      Spacer()
+    }.padding(20.0)
   }
 }
 
 struct AboutView_Previews: PreviewProvider {
   static var previews: some View {
-    AboutView()
+    TabView {
+      NavigationView {
+        ZStack {
+          AboutView()
+        }.navigationBarTitle("About")
+      }.navigationViewStyle(StackNavigationViewStyle()).tabItem {
+        Image("far.thumbs-up").renderingMode(.template)
+        Text("About")
+      }.tag(3)
+    }
   }
 }

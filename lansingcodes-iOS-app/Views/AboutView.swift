c899a51f47a06df5057d057e9253c9a5ff1d6045
Leo Dion <leogdion@brightdigit.com>
2019-12-06 17:04:54 -0500

    updated version number, working on about view

--- lansingcodes-iOS-app/Views/AboutView.swift
+++ lansingcodes-iOS-app/Views/AboutView.swift
@@ -4,18 +4,33 @@ struct AboutView: View {
   var body: some View {
     VStack {
       Spacer()
-      Image("Logo").resizable().scaledToFit().frame(minHeight: 40.0, maxHeight: 80.0)
-      Text("LANSING CODES").font(.title)
-      VStack(alignment: .center) {
-        Text("Events and resources for Lansing coders")
-        Divider()
-        Text("For those who code or aspire to, professionally or as a hobby").font(.caption).multilineTextAlignment(.center)
-      }.padding(20.0)
-      Text("Developed By")
-      Text("Leo Dion")
-      Image("BrightDigit").renderingMode(.template).resizable().scaledToFit().frame(minHeight: 40.0, maxHeight: 80.0)
+      VStack {
+        Image("Logo").resizable().scaledToFit().frame(minHeight: 40.0, maxHeight: 80.0)
+        Text("LANSING CODES").font(.title)
+        VStack(alignment: .center) {
+          Text("Events and resources for Lansing coders")
+
+          Divider()
+          Text("For those who code or aspire to, professionally or as a hobby").font(.caption).multilineTextAlignment(.center)
+        }
+      }
+      Spacer(minLength: 4)
+      VStack {
+        Text("Join the Discussion at our Slack").font(.caption).multilineTextAlignment(.center)
+        Image("fab.slack").renderingMode(.template).resizable().scaledToFit().frame(minHeight: 30.0, maxHeight: 60.0)
+      }
       Spacer()
-      Text("Version: 1.0-beta.4 (4)").font(.caption).foregroundColor(.gray)
+      VStack {
+        Text("Developed By")
+        Text("Leo Dion")
+        Image("BrightDigit").renderingMode(.template).resizable().scaledToFit().frame(minHeight: 40.0, maxHeight: 80.0)
+      }
+      Spacer()
+      VStack {
+        Text("Submit Your Feedback or Contribute to the App")
+        Image("fab.github").renderingMode(.template).resizable().scaledToFit().frame(minHeight: 30.0, maxHeight: 60.0)
+        Text("Version: 1.0-beta.5 (5)").font(.caption).foregroundColor(.gray)
+      }
       Spacer()
     }.padding(20.0)
   }

6bd5d481712fc08dfaf395f42769d8cfefa80a46
Leo Dion <leogdion@brightdigit.com>
2019-12-06 20:01:18 -0500

    working on adding action sheet

--- lansingcodes-iOS-app/Views/AboutView.swift
+++ lansingcodes-iOS-app/Views/AboutView.swift
@@ -1,19 +1,35 @@
 import SwiftUI
 
+struct SheetDestination: Identifiable {
+  let url: URL
+  let name: String
+
+  var id: String {
+    return name
+  }
+}
+
 struct AboutView: View {
+  @State var sheetDestination: SheetDestination?
+
   var body: some View {
     VStack {
       Spacer()
-      VStack {
-        Image("Logo").resizable().scaledToFit().frame(minHeight: 40.0, maxHeight: 80.0)
-        Text("LANSING CODES").font(.title)
-        VStack(alignment: .center) {
-          Text("Events and resources for Lansing coders")
+      Button(action: {
+        self.sheetDestination = SheetDestination(url: URL(string: "https://lansing.codes")!, name: "Lansing Codes WebSite")
+      }) {
+        VStack {
+          Image("Logo").resizable().scaledToFit().frame(minHeight: 40.0, maxHeight: 80.0)
+          Text("LANSING CODES").font(.title)
+          VStack(alignment: .center) {
+            Text("Events and resources for Lansing coders")
 
-          Divider()
-          Text("For those who code or aspire to, professionally or as a hobby").font(.caption).multilineTextAlignment(.center)
+            Divider()
+            Text("For those who code or aspire to, professionally or as a hobby").font(.caption).multilineTextAlignment(.center)
+          }
         }
       }
+
       Spacer(minLength: 4)
       VStack {
         Text("Join the Discussion at our Slack").font(.caption).multilineTextAlignment(.center)
@@ -32,7 +48,10 @@ struct AboutView: View {
         Text("Version: 1.0-beta.5 (5)").font(.caption).foregroundColor(.gray)
       }
       Spacer()
-    }.padding(20.0)
+    }
+    /*.actionSheet(item: $sheetDestination, content: {
+      ActionSheet(title: "Hello")
+    })*/
   }
 }
 
